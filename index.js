9a824f7c546ba0f9af015827e7f0ceb90f0cc819
Kevin Lindsey <kevin@pixate.com>
2013-08-01 21:34:12 -0500

    Re-export Point2D from kld-affine module

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// expose classes
+// expose module classes
 
 exports.Intersection = require('./lib/Intersection');
 exports.IntersectionParams = require('./lib/IntersectionParams');
+
+// expose affine module classes
+exports.Point2D = require('kld-affine').Point2D;
