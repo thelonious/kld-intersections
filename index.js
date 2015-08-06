78dab27b0a25fe787486eed9042d6c799e735726
Jan-Felix <schwarz@mpango.de>
2015-08-06 16:52:19 +0200

    simplified the API, modularized the lib, added full path support

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // expose module classes
 
-exports.Intersection = require('./lib/Intersection');
+exports.intersect = require('./intersect');
 exports.IntersectionParams = require('./lib/IntersectionParams');
+exports.shapeFunctions = require('./lib/functions/shape');
+exports.bezierFunctions = require('./lib/functions/bezier');
 
 // expose affine module classes
 exports.Point2D = require('kld-affine').Point2D;
