90445023df45adf5bb9a1dd83f8aed288822db3d
Kevin Lindsey <kevin@kevlindev.com>
2017-07-18 11:10:27 -0700

    Expose Vector2D and Matrix2D
    This exposes access to classes from kld-affine and fixes #12.

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ exports.IntersectionParams = require('./lib/IntersectionParams');
 
 // expose affine module classes
 exports.Point2D = require('kld-affine').Point2D;
+exports.Vector2D = require('kld-affine').Vector2D;
+exports.Matrix2D = require('kld-affine').Matrix2D;
