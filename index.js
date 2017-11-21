4f97aac5995c44632a9a669f8e2570ee833042bc
Kevin Lindsey <kevin@kevlindev.com>
2017-11-20 16:55:34 -0800

    Expose IntersectionQuery

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ exports.Intersection = require('./lib/Intersection');
 exports.IntersectionArgs = require('./lib/IntersectionArgs');
 exports.Shapes = require('./lib/Shapes');
 exports.AffineShapes = require('./lib/AffineShapes');
+exports.IntersectionQuery = require('./lib/IntersectionQuery');
 
 // expose affine module classes
 exports.Point2D = require('kld-affine').Point2D;
