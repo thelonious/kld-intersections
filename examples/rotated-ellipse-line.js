7766c9aa7cffe1e8d37eff18325475408c44a1a4
Kevin Lindsey <kevin@kevlindev.com>
2018-05-26 16:05:44 -0700

    Show how to map back to the original coordinate system

--- examples/rotated-ellipse-line.js
+++ examples/rotated-ellipse-line.js
@@ -13,7 +13,7 @@ const line = {
     p2: new Point2D(70, 150)
 };
 
-// define ellipse
+// define rotated ellipse
 const ellipse = {
     center: new Point2D(110, 60),
     radiusX: 100,
@@ -21,24 +21,33 @@ const ellipse = {
     angle: 10
 };
 
-// define rotation in radians
-const angle = ellipse.angle * Math.PI / 180.0;
-
-// create matrix for rotating around center of rectangle
-const rotation = Matrix2D.rotation(-angle);
-
+// rotate the line into the ellipse's axis-aligned coordinate system
+const radians = ellipse.angle * Math.PI / 180.0;
+const rotation = Matrix2D.rotation(-radians);
 const rotated_line = {
     p1: line.p1.transform(rotation),
     p2: line.p2.transform(rotation)
 };
 
 // find intersections
-const result = Intersection.intersectEllipseLine(ellipse.center, ellipse.radiusX, ellipse.radiusY, rotated_line.p1, rotated_line.p2);
+const result = Intersection.intersectEllipseLine(
+    ellipse.center, ellipse.radiusX, ellipse.radiusY,
+    rotated_line.p1, rotated_line.p2
+);
 
 // build SVG file showing the shapes, the center point, and intersection points
 const intersectionSVG = result.points.map(p => {
     return `<circle cx="${p.x.toFixed(3)}" cy="${p.y.toFixed(3)}" r="2" stroke="red" fill="none"/>`
 }).join("\n    ");
+
+//
+// NOTE: This SVG transforms the intersection points back into the original
+// coordinate system. If you need to do that in your code, use something like
+// the following:
+//
+// const unrotation = Matrix2D.rotation(radians);
+// const unrotated_points = result.points(p => p.transform(unrotation));
+//
 
 const svg = `<svg xmlns="http://www.w3.org/2000/svg">
   <g transform="translate(10,10)">
