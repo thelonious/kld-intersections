b9d0cbec66ffb32d7e59a3766c5aae2b4c46129b
Brett Zamir <brettz9@yahoo.com>
2019-05-05 07:51:57 +0800

    - npm: Add `eslint` to test script - npm: Add scripts/server for building and opening JSDoc - npm: Add to ignore file

---
+++ docs/jsdoc-config.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = {
+    plugins: [],
+    recurseDepth: 10,
+    source: {
+        exclude: [
+            "node_modules",
+            "dist",
+            "test"
+        ],
+        excludePattern: "rollup*"
+    },
+    sourceType: "module",
+    tags: {
+        allowUnknownTags: false
+    },
+    templates: {
+        cleverLinks: true,
+        monospaceLinks: false
+    },
+    opts: {
+        recurse: true,
+        verbose: true,
+        destination: "docs/jsdoc"
+    }
+};
