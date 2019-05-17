cb86cd23d0e7d6ff92bfd3af213f4364287e11f0
Kevin Lindsey <kevin@kevlindev.com>
2019-05-17 16:46:59 -0700

    Saving last changes before pulling in kld-data-normalizer

--- scratchpad/shapeInfo-object-test.js
+++ scratchpad/shapeInfo-object-test.js
@@ -3,31 +3,31 @@
 import {Intersection, ShapeInfo} from "../index.js";
 import {Point2D} from "kld-affine";
 
-const circle = ShapeInfo.fromObject({
+const circle = ShapeInfo.object({
     type: ShapeInfo.CIRCLE,
     center: new Point2D(80, 80),
     radius: 40
 });
-const ellipse = ShapeInfo.fromObject({
+const ellipse = ShapeInfo.object({
     type: ShapeInfo.ELLIPSE,
     center: new Point2D(80, 80),
     radiusX: 30,
     radiusY: 40
 });
-const line = ShapeInfo.fromObject({
+const line = ShapeInfo.object({
     type: ShapeInfo.LINE,
     p1: new Point2D(10, 10),
     p2: new Point2D(110, 110)
 });
 // NOTE: path covers arc, bezier2, and bezier3. These will probably never be created directly. Instead
 // PathParser/PathHandler will most likely be used either directly or indirectly.
-const path = ShapeInfo.fromObject({
+const path = ShapeInfo.object({
     type: ShapeInfo.PATH,
     segments: [
-        ShapeInfo.fromObject({type: ShapeInfo.LINE, p1: new Point2D(10, 10), p2: new Point2D(110, 110)})
+        ShapeInfo.object({type: ShapeInfo.LINE, p1: new Point2D(10, 10), p2: new Point2D(110, 110)})
     ]
 });
-const polygon = ShapeInfo.fromObject({
+const polygon = ShapeInfo.object({
     type: ShapeInfo.POLYGON,
     points: [
         new Point2D(10, 20),
@@ -36,7 +36,7 @@ const polygon = ShapeInfo.fromObject({
         new Point2D(10, 100)
     ]
 });
-const rectangle = ShapeInfo.fromObject({
+const rectangle = ShapeInfo.object({
     type: ShapeInfo.RECTANGLE,
     topLeft: new Point2D(35, 25),
     bottomRight: new Point2D(80, 100)
