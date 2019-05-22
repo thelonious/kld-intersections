803124c7168bb61d61480ff8e389a2f96d8a540a
Kevin Lindsey <kevin@kevlindev.com>
2019-05-21 21:07:53 -0700

    Switch to path method

--- scratchpad/cubic-cubic-fail.js
+++ scratchpad/cubic-cubic-fail.js
@@ -7,8 +7,8 @@ const pathData1 = "M150,150 C183.33333333333331,216.66666666666663 233.333333333
 const pathData2 = "M100,200 C166.66666666666663,133.33333333333337 233.33333333333337,133.33333333333337 300,200";
 
 // create pathS
-const path1 = Shapes.pathData(pathData1);
-const path2 = Shapes.pathData(pathData2);
+const path1 = Shapes.path(pathData1);
+const path2 = Shapes.path(pathData2);
 
 // intersect
 const result = Intersection.intersect(path1, path2);
